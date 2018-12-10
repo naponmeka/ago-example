665ef3ba87d03d6122f4e832a7cc277a5bd6687f
Napon Mekavuthikul <napon.meka@gmail.com>
2018-12-10 14:04:45 +0700

    update code + use simple routing

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"syscall/js"
 	"time"
 
@@ -20,7 +21,14 @@ type TodoPageData struct {
 func main() {
 	println("WASM Go Initialized.")
 	doc := js.Global().Get("document")
-	doc.Set("title", "Ago example")
+	doc.Set("title", "Ago framework example")
+	rootDom := js.Global().Get("document").Call("getElementById", "root")
+	agoRouter := ago.Router{
+		Mode:    "HISTORY_MODE",
+		Root:    "/",
+		RootDom: js.Global().Get("document").Call("getElementById", "root"),
+	}
+
 	gox := `
 	<div>
 		<h1>{{.PageTitle}}</h1>
@@ -44,52 +52,77 @@ func main() {
 			{Title: "Task 2", Done: false},
 		},
 	}
+	fmt.Println("create component")
 	agoComponent := ago.CreateComponent(gox, state)
-	agoComponent.Render("root")
+	fmt.Println("render")
+	agoComponent.Render(rootDom)
 	state2 := TodoPageData{
 		PageTitle: "My TODO list2",
 		Todos: []Todo{
-			{Title: "Task 0", Done: false},
+			{Title: "Task 0", Done: true},
 			{Title: "Task 1", Done: false},
 			{Title: "Task 2", Done: false},
 		},
 	}
 	time.Sleep(2 * time.Second)
+	fmt.Println("change state to 2")
 	agoComponent.ChangeState(state2)
-	state3 := TodoPageData{
-		PageTitle: "My TODO list3",
-		Todos: []Todo{
-			{Title: "Task 1", Done: false},
-			{Title: "Task 2", Done: false},
-			{Title: "Task 3", Done: false},
-			{Title: "Task 4", Done: false},
-			{Title: "Task 5", Done: false},
-			{Title: "Task 6", Done: false},
-			{Title: "Task 7", Done: false},
-		},
-	}
-	time.Sleep(5 * time.Second)
-	agoComponent.ChangeState(state3)
-	state4 := TodoPageData{
-		PageTitle: "My TODO list4",
-		Todos: []Todo{
-			{Title: "Task x", Done: false},
-			{Title: "Task y", Done: false},
-			{Title: "Task z", Done: false},
-		},
-	}
-	time.Sleep(5 * time.Second)
-	agoComponent.ChangeState(state4)
-	state5 := TodoPageData{
-		PageTitle: "My TODO list5",
-		Todos: []Todo{
-			{Title: "Task x", Done: false},
-			{Title: "Task 1", Done: false},
-			{Title: "Task 2", Done: false},
-			{Title: "Task 3", Done: false},
-		},
-	}
-	time.Sleep(5 * time.Second)
-	agoComponent.ChangeState(state5)
+	time.Sleep(2 * time.Second)
+	detailComponent := ago.CreateComponent(`
+	<div>
+		<p>some details</p>
+	</div>
+	`, nil)
+	detailComponent.Render(agoComponent.VDom.Dom)
+
+	agoRouter.AddRoute(ago.Route{
+		Path:      "todo",
+		Component: agoComponent,
+	})
+	agoRouter.AddRoute(ago.Route{
+		Path:      "details",
+		Component: detailComponent,
+	})
+
+	time.Sleep(2 * time.Second)
+	agoRouter.Navigate("xxx")
+	time.Sleep(2 * time.Second)
+	agoRouter.Navigate("todo")
+
+	// state3 := TodoPageData{
+	// 	PageTitle: "My TODO list3",
+	// 	Todos: []Todo{
+	// 		{Title: "Task 1", Done: false},
+	// 		{Title: "Task 2", Done: false},
+	// 		{Title: "Task 3", Done: false},
+	// 		{Title: "Task 4", Done: false},
+	// 		{Title: "Task 5", Done: false},
+	// 		{Title: "Task 6", Done: false},
+	// 		{Title: "Task 7", Done: false},
+	// 	},
+	// }
+	// time.Sleep(2 * time.Second)
+	// agoComponent.ChangeState(state3)
+	// state4 := TodoPageData{
+	// 	PageTitle: "My TODO list4",
+	// 	Todos: []Todo{
+	// 		{Title: "Task x", Done: false},
+	// 		{Title: "Task y", Done: false},
+	// 		{Title: "Task z", Done: false},
+	// 	},
+	// }
+	// time.Sleep(2 * time.Second)
+	// agoComponent.ChangeState(state4)
+	// state5 := TodoPageData{
+	// 	PageTitle: "My TODO list5",
+	// 	Todos: []Todo{
+	// 		{Title: "Task x", Done: false},
+	// 		{Title: "Task 1", Done: false},
+	// 		{Title: "Task 2", Done: false},
+	// 		{Title: "Task 3", Done: false},
+	// 	},
+	// }
+	// time.Sleep(2 * time.Second)
+	// agoComponent.ChangeState(state5)
 
 }
